47969cdabc323a165db6c9dcb4504c553c8a1ec1
Darci Junio Soares Rodrigues <darcijsrodrigues@outlook.com.br>
2017-12-05 21:48:37 -0200

    commit

---
+++ SampleNavigationDrawerLayout/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+using V7Toolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.V4.Widget;
+using Android.Support.Design.Widget;
+using System;
+
+namespace SampleNavigationDrawerLayout
+{
+    [Activity(Label = "SampleNavigationDrawerLayout", Theme = "@style/Theme.DesignDemo", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        DrawerLayout drawerLayout;
+        NavigationView navigationView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+
+            var toolbar = FindViewById<V7Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            var drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, Resource.String.drawer_open, Resource.String.drawe_close);
+            drawerLayout.SetDrawerListener(drawerToggle);
+            drawerToggle.SyncState();
+            navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            setupDrawerContent(navigationView);
+        }
+
+        private void setupDrawerContent(NavigationView navigationView)
+        {
+            navigationView.NavigationItemSelected += (sender, e) => {
+                e.MenuItem.SetChecked(true);
+                drawerLayout.CloseDrawers();
+            };
+        }
+
+        public override bool OnCreateOptionsMenu(Android.Views.IMenu menu)
+        {
+            navigationView.InflateMenu(Resource.Menu.nav_menu);
+            return true;
+        }
+    }
+}
+
